96065adff7356c5907d39e473f37e93006af8534
solitonyc <solito-air@ool-ad0204bf.dyn.optonline.net>
2019-04-18 17:04:53 -0400

    lengthen page width and add id anchor for nav

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,7 +4,7 @@ import './Menu.css';
 export default class Menu extends Component {
   render() {
     return (
-        <div className='menu_main_container'>
+        <div className='menu_main_container' id='menu'>
             <div className='menu_headline_section'>
                 <h1><span>MENU</span></h1>
             </div>
@@ -216,6 +216,7 @@ export default class Menu extends Component {
                             <i className='item'>Grilled Chicken</i>
                             <i className="price"> $13.00</i> 
                         </li>
+                        delete this
                     </ul>
                 </div>
             </div>
