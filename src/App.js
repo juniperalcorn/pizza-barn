a6ebd31ec9e320cc0de9e0399531243d6ff1ee29
Sean Mercer <seansmercer@gmail.com>
2019-04-18 17:12:43 -0400

    more small tweaks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/Header.js'
 import CaterHistoryNews from './components/caterHistoryNews/caterHistoryNews.js'
 import Media from './components/media/Media'
+import Menu from './components/menu/Menu'
 
 class App extends Component {
   render() {
