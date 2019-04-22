c06927d0e34f86ac1ac097c42277e6379b361437
Alane <bibang007@gmail.com>
2019-04-22 11:35:08 -0400

    as close as i cant get

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,10 +7,10 @@ import slicePizzaBackMobile from '../../assets/images/heroMobile480.png'
 export default class Header extends Component {
   render() {
     return (
-        <div className='header-page'>
-        <img className="backImage"src={slicePizzaBack}/>
-        <img className="backImageMobile"src={slicePizzaBackMobile }/>
-          <div className='header'>
+      <div className='header-page'>
+        <img className="backImage" src={slicePizzaBack} />
+        <img className="backImageMobile" src={slicePizzaBackMobile} />
+        <div className='header'>
           <div className='links'>
             <a className='home' href="#menu">MENU</a>
 
@@ -26,43 +26,81 @@ export default class Header extends Component {
 
         <div className='footer'>
 
-        <div className='address'>
+          <div className='address'>
 
             <p>(914) 378-1400
-            <br/>
-            70 McLean Avenue
-            <br/>
-            Yonkers, NY 10705
+            <br />
+              70 McLean Avenue
+            <br />
+              Yonkers, NY 10705
            </p>
 
-        </div>
+          </div>
 
-        <div className='hours'>
-             
-              <p>HOURS
-                <br/>
-                  Mon-Sat   11AM-10PM
-                <br/>
-                  Sunday   Closed
+          <div className='hours'>
+
+            <p>HOURS
+                <br />
+              Mon-Sat   11AM-10PM
+                <br />
+              Sunday   Closed
               </p>
 
+          </div>
+
+          <div className='text'>
+
+            <p>
+              Phone orders to-go only and will be suspended <span className="not-bold">when a line forms<br />
+                outside the restaurant.</span><br />
+              In-house seats are reserved for our dining-in patrons only <span className="not-bold">and <br />
+                patrons will be seated on a first serve basis.</span>
+            </p>
+          </div>
+
+
         </div>
 
-        <div className='text'>
-    
-        <p>
-        Phone orders to-go only and will be suspended <span className="not-bold">when a line forms<br />
-        outside the restaurant.</span><br/>
-        In-house seats are reserved for our dining-in patrons only <span className="not-bold">and <br />
-        patrons will be seated on a first serve basis.</span>
-        </p>
+        <div className='footerMobile'>
+
+          <div className='fuckingDiv'>
+            <div className='addressMobile'>
+
+              <p>(914) 378-1400
+    <br />
+                70 McLean Avenue
+    <br />
+                Yonkers, NY 10705
+   </p>
+
+            </div>
+
+            <div className='hoursMobile'>
+
+              <p>HOURS
+        <br />
+                Mon-Sat   11AM-10PM
+        <br />
+                Sunday   Closed
+      </p>
+            </div>
+          </div>
+
+          <div className='textMobile'>
+
+            <p>
+              Phone orders to-go only and will be suspended <span className="not-bold">when a line forms<br />
+                outside the restaurant.</span><br />
+              In-house seats are reserved for our dining-in patrons only <span className="not-bold">and <br />
+                patrons will be seated on a first serve basis.</span>
+            </p>
+          </div>
+
+
         </div>
-     
-        
-       </div> 
 
 
-       </div>
+      </div>
     )
   }
 }
